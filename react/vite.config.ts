a3070d0d64a6dcb49a96f62fbd9e10863acb78e2
arebusphan <160636002+arebusphan@users.noreply.github.com>
2025-05-30 00:35:16 +0700

    react seting

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,9 +1,17 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [plugin(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     server: {
         port: 53525,
     }
