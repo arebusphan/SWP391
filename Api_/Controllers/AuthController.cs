c9abdc87346494c4a5c0b24e82dba8927e2476e4
Trần Đình Tân <tantdse181727@fpt.edu.vn>
2025-05-29 20:52:04 +0700

    authService

--- Api_/Controllers/AuthController.cs
+++ Api_/Controllers/AuthController.cs
@@ -1,6 +1,45 @@
-﻿namespace API_.Controllers
+﻿using BLL.AuthService;
+using DAL;
+using DAL.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication6.controllers
 {
-    public class AuthController
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
+
+        private readonly IAuthService _authService;
+
+        public AuthController(
+            IAuthService authService,
+            IConfiguration configuration
+
+            )
+        {
+
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
+        {
+            var (success, message, user) = await _authService.LoginUserAsync(loginDTO);
+            if (success)
+            {
+                string token = _authService.CreateToken(user);
+                return Ok(new { message, token });
+            }
+            else
+            {
+                return BadRequest(new { message });
+            }
+        }
+
     }
 }
