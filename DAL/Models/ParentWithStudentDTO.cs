8d42ebd09cde7c6fd6d94a5e928662342725add4
Hopnguyen21 <hopnguyen2101@gmail.com>
2025-06-10 10:39:00 +0700

    fix add another user
    sửa student có thể null

--- DAL/Models/ParentWithStudentDTO.cs
+++ DAL/Models/ParentWithStudentDTO.cs
@@ -9,7 +9,7 @@ namespace DAL.Models
     public class ParentWithStudentDTO
     {
         public UserDTO Parent { get; set; }
-        public StudentAddDTO Student { get; set; }
+        public StudentAddDTO? Student { get; set; }
     }
 
 }
