27c243e804480249ae2d53177851facd9e59fc2f
arebusphan <160636002+arebusphan@users.noreply.github.com>
2025-07-05 19:54:03 +0700

    fix layout

--- react/src/components/layout.tsx
+++ react/src/components/layout.tsx
@@ -1,16 +1,33 @@
+﻿import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './navbar';
-import { Outlet } from 'react-router-dom';
 import Footer from './footer';
+
 const Layout = () => {
+    const location = useLocation();
+
+
+    const hideFooterRoutes = [
+        "/dashboard",
+        "/ParentPage",
+        "/MedicalStaffPage",
+        "/AdminPage",
+        "/ManagerPage"
+    ];
+
+
+    const shouldHideFooter = hideFooterRoutes.some((route) =>
+        location.pathname.startsWith(route)
+    );
+
     return (
-        <div>
+        <div className="min-h-screen flex flex-col">
             <Navbar />
-            <main>
+            <main className="flex-grow">
                 <Outlet />
             </main>
-            <Footer />
-
+            {!shouldHideFooter && <Footer />}
         </div>
-    )
-}
+    );
+};
+
 export default Layout;
