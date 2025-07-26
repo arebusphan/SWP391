589e888541ce20685328c581b887e238e414583a
arebusphan <160636002+arebusphan@users.noreply.github.com>
2025-07-26 18:31:52 +0700

    .

--- DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c87b17c3013c60c473bb3dd08d0cbec9fb4308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec3693296131437b87410e27b2eb180d606d887")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
